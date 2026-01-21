d77fa98bc26973d1b857c89ae9a14c3d909266ae
CDFelix-edu <d.calarese@studenti.unimol.it>
2026-01-21 04:05:05 +0100

    H2 integration

--- src/test/java/it/unimol/taxManager/TaxManagerApplicationTests.java
+++ src/test/java/it/unimol/taxManager/TaxManagerApplicationTests.java
@@ -2,6 +2,8 @@ package it.unimol.taxManager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
 @ActiveProfiles("test")
